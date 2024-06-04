87870475511c840d3c49b4b408fb44da37e3f5ce
Shreeya1904 <shreeya1904@gmail.com>
2024-06-04 20:12:39 +0530

    main file update and main.o add

--- calculator_project/src/main.c
+++ calculator_project/src/main.c
@@ -6,8 +6,44 @@
 #include "../inc/div.h"
 #include "../inc/menu.h"
 
-int main()
-{
+void printCalculatorMenu();
+
+int main() {
+    int choice;
+    int32_t num1, num2;
+
     printf("\n\rWelcome to the Calculator Project\n\r");
+
+    while (1) {
+        printCalculatorMenu();
+        scanf("%d", &choice);
+        
+        if (choice < 1 || choice > 4) {
+            printf("Invalid choice! Please select again.\n");
+            continue;
+        }
+
+        printf("Enter two numbers: ");
+        scanf("%d %d", &num1, &num2);
+
+        switch (choice) {
+            case 1:
+                printf("Result: %d\n", addition(num1, num2));
+                break;
+            case 2:
+                printf("Result: %d\n", subtraction(num1, num2));
+                break;
+            case 3:
+                printf("Result: %d\n", multiplication(num1, num2));
+                break;
+            case 4:
+                printf("Result: %d\n", division(num1, num2));
+                break;
+            default:
+                printf("Invalid choice! Please select again.\n");
+                break;
+        }
+    }
+
     return 0;
 }
