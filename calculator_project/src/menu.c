ee5df185cdbd4df80ea7a991d3022e80ba072bf9
Ishika <bansalishika050304@gmail.com>
2024-06-04 22:51:27 +0530

    changes made regarding add function

--- calculator_project/src/menu.c
+++ calculator_project/src/menu.c
@@ -3,6 +3,6 @@
 void printCalculatorMenu()
 {
     printf("press a kwy according to the given menu:\n");
-    printf("press 2 for subtract :\n press 3 for multiply :\n");
+    printf("press 1 for addition :\n press 2 for subtract :\n press 3 for multiply :\n");
 
 }
