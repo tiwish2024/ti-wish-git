48f2c97b8186d1d25eb7bf182d3eb4c08955e54a
Mayura <saimayurark@gmail.com>
2024-06-04 19:44:43 +0530

    Final commit

--- calculator_project/src/main.c
+++ calculator_project/src/main.c
@@ -6,8 +6,33 @@
 #include "../inc/div.h"
 #include "../inc/menu.h"
 
+
+
 int main()
 {
+    int32_t num1,num2,choice;
+    
     printf("\n\rWelcome to the Calculator Project\n\r");
+    while(){
+        printf("Enter num1 and num2 : ");
+        scanf("%d %d",&num1,&num2);
+        printCalculatorMenu();
+        scanf("%d",&choice);
+        if (choice ==1){
+            printf("Result is : %d",addition(num1,num2));
+        }
+        else if (choice ==2){
+            printf("Result is : %d",subtraction(num1,num2));
+        }
+        else if (choice ==3){
+            printf("Result is : %d",division(num1,num2));
+        }
+        else if (choice ==4){
+            printf("Result is : %d",multiplication(num1,num2));
+        }
+        else{
+            printf("Invalid choice\n");
+        }
+    }
     return 0;
 }
