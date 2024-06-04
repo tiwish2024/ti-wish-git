a1437d01153266ee1efa761f11c86c0ae1edc881
Keerthana <keerthana.s2022d@vitstudent.ac.in>
2024-06-04 15:33:28 +0530

    Change div.c and menu.c

--- calculator_project/src/div.c
+++ calculator_project/src/div.c
@@ -2,5 +2,6 @@
 
 int32_t division(int32_t num1, int32_t num2)
 {
-    
+    if(num2!=0)
+        return num1/num2;
 }
--- calculator_project/src/menu.c
+++ calculator_project/src/menu.c
@@ -5,4 +5,5 @@ void printCalculatorMenu()
     printf("1.Addition\n");
     printf("2. Subtraction\n");
     printf("3. Multiply\n");
+    printf("4. Division\nEnter Choice: ");
 }
