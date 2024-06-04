4384cdcac155d37bfcbd68789354f92f63ad8ce5
balwani <amisha.balwani.ug22@nsut.ac.in>
2024-06-04 19:23:28 +0530

    Resolve conflict 3

--- calculator_project/src/menu.c
+++ calculator_project/src/menu.c
@@ -3,6 +3,9 @@
 void printCalculatorMenu()
 {
    printf("Choose 1 for addition\n");
+   printf("choose 2 for subtraction");
    printf("Choose 3 for division\n");    
    printf("Choose 4 for multiplication\n");
 }
+
+
